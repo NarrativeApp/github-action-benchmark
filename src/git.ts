73f8b66749dc34bb17c18acb7e579413412465e5
Stijn Seghers <stijn.seghers@narrative.so>
2021-02-16 15:42:11 +1300

    Remove uses of event payload

--- src/git.ts
+++ src/git.ts
@@ -53,14 +53,8 @@ export async function cmd(...args: string[]): Promise<string> {
 }
 
 function getRemoteUrl(token: string): string {
-    /* eslint-disable @typescript-eslint/camelcase */
-    const fullName = github.context.payload.repository?.full_name;
-    /* eslint-enable @typescript-eslint/camelcase */
-
-    if (!fullName) {
-        throw new Error(`Repository info is not available in payload: ${JSON.stringify(github.context.payload)}`);
-    }
-
+    const repo = github.context.repo;
+    const fullName = `${repo.owner}/${repo.repo}`;
     return `https://x-access-token:${token}@github.com/${fullName}.git`;
 }
 
